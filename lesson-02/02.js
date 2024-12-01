5e2cf883bf058aa35fbd7636492d8ccf7f8b2668
trinner3 <164670497+trinner3@users.noreply.github.com>
2024-12-01 14:04:58 +0300

    02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,16 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(1 + Math.random() * 6) // your code
+let dice2 = Math.floor(1 + Math.random() * 6)// your code
+let isWinningDouble = false // your code
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
+if (dice1 === dice2) {
+  isWinningDouble = true
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
