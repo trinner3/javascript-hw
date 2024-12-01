b227917e0129925c15342c899f78189d38e12aa4
trinner3 <164670497+trinner3@users.noreply.github.com>
2024-12-01 14:08:13 +0300

    02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -14,7 +14,7 @@ let isWinningDouble = false // your code
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (dice1 === dice2) {
+if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
   isWinningDouble = true
   console.log('Выигрышный дубль!')
 } else {
