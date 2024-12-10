d63526733bbe7d8438cad0d1c8e5a39aeb732522
trinner3 <164670497+trinner3@users.noreply.github.com>
2024-12-10 18:50:03 +0300

    01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(num1, num2) {
+    return (num1 + num2)/2;
+}
