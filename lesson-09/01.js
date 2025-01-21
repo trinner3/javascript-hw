f1c06f522c9ef366ab4e3b8a2bb4121458949a58
trinner3 <164670497+trinner3@users.noreply.github.com>
2025-01-21 23:18:13 +0300

    01.js

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -22,7 +22,7 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
-
+const array = [1, 2, 3, 4, 5]
 const filter = array.filter((element, index) => {
   return element % 2 !== 0
 });
